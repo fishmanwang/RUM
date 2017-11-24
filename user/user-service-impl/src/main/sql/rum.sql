d494d37d608828b0f4a06477bf273ce3158adad7
wangtianjing <wangtianjing@bbdservice.com>
2017-11-24 18:48:58 +0800

    集成pagehelper

---
+++ user/user-service-impl/src/main/sql/rum.sql
@@ -0,0 +1,83 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.12-log : Database - rum
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rum` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `rum`;
+
+/*Table structure for table `rum_account` */
+
+DROP TABLE IF EXISTS `rum_account`;
+
+CREATE TABLE `rum_account` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(64) DEFAULT NULL,
+  `phone` varchar(16) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `region` varchar(16) DEFAULT NULL,
+  `dep_note` varchar(64) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='账户表';
+
+/*Table structure for table `rum_permission` */
+
+DROP TABLE IF EXISTS `rum_permission`;
+
+CREATE TABLE `rum_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `code` varchar(64) DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='权限表';
+
+/*Table structure for table `rum_user` */
+
+DROP TABLE IF EXISTS `rum_user`;
+
+CREATE TABLE `rum_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `rum_user_permission` */
+
+DROP TABLE IF EXISTS `rum_user_permission`;
+
+CREATE TABLE `rum_user_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户权限';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
